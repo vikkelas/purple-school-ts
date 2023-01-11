ec0894971632a3ef3f2083a4e3bfa67a38163574
vikkelas <bass89@mail.ru>
2023-01-11 17:44:59 +0300

    refactor code

--- hw-2/app.ts
+++ hw-2/app.ts
@@ -1,84 +1,92 @@
-import axios, {AxiosError} from "axios";
+import axios, {AxiosError, AxiosResponse} from "axios";
 
 interface Response {
-    users: User[]
-    total: number
-    skip: number
-    limit: number
+    users: User[];
+    total: number;
+    skip: number;
+    limit: number;
 }
 
-enum gender {
+enum GENDER {
     FEMALE='female',
     MALE='male'
 }
 
-interface User {
-    id: number,
-    firstName: string
-    lastName: string
-    maidenName: string
-    age: number,
-    gender: gender
-    email: string
-    phone: string
-    username: string
-    password: string
-    birthDate: string
-    image: string
-    bloodGroup: string
-    height: number
-    weight: number
-    eyeColor: string
-    hair: {
-        color: string
-        type: string
-    },
-    domain: string
-    ip: string
-    address: Address
-    macAddress: string
-    university: string
-    bank: Bank
-    company: Company
-    ein: string
-    ssn: string
-    userAgent: string
+enum BLOOD_GROUP {
+    GroupA='A',
+    GroupB='B',
+    GroupAB='AB',
+    GroupO='O',
+    GroupAPlus='A+',
+    GroupBPlus='B+',
+    GroupABPlus='AB+',
+    GroupOPlus='O+'
 }
 
 interface Bank {
-    cardExpire: string
-    cardNumber: string
-    cardType: string
-    currency: string
-    iban: string
+    cardExpire: string;
+    cardNumber: string;
+    cardType: string;
+    currency: string;
+    iban: string;
 }
 
 interface Address {
-    address: string
-    city: string
+    address: string;
+    city: string;
     coordinates: {
-        lat: number
-        lng: number
+        lat: number;
+        lng: number;
     }
-    postalCode: string
-    state: string
-    macAddress: string
-    university: string
+    postalCode: string;
+    state: string;
 }
 
 interface Company {
-    address: Address
-    department: string
-    name: string
-    title: string
+    address: Address;
+    department: string;
+    name: string;
+    title: string;
 }
 
-const URL = 'https://dummyjson.com/users'
+interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    maidenName: string;
+    age: number;
+    gender: GENDER;
+    email: string;
+    phone: string;
+    username: string;
+    password: string;
+    birthDate: string;
+    image: string;
+    bloodGroup: BLOOD_GROUP;
+    height: number;
+    weight: number;
+    eyeColor: string;
+    hair: {
+        color: string;
+        type: string;
+    },
+    domain: string;
+    ip: string;
+    address: Address;
+    macAddress: string;
+    university: string;
+    bank: Bank;
+    company: Company;
+    ein: string;
+    ssn: string;
+    userAgent: string;
+}
 
-async function getUsers(){
+const URL: string = 'https://dummyjson.com/users'
+
+async function getUsers(url: string){
     try{
-        const {users} = await axios.get<Response>(URL).then(response=> response.data)
-        return users;
+        return  await axios.get<AxiosResponse>(url).then(response=> response.data)
     }catch (error){
         if(error as AxiosError){
             console.log(error)
